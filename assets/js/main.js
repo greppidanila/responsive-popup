0eba79eb329bb4e4a3fe5edf5323ba4bac7c315d
Danila Greppi <111403403+greppidanila@users.noreply.github.com>
2023-11-05 17:00:41 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,11 @@ const showModal = (openButton, modalContent) =>{
 }
 showModal('open-modal','modal-container')
 
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
